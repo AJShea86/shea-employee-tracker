d171cd874f124f54f08dc27df280e30d898061a6
Andrew S <andrewsheamls@gmail.com>
2022-03-15 17:29:54 -0600

    added a seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES()
+
+
+
+
+INSERT INTO role (id, title, salary, dept_id)
+VALUES()
+
+
+
+
+INSERT INTO dept (id, dept_name)
+VALUES()
