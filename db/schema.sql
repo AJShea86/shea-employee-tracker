4c6c615fb9f150a1a30b2700adf05c19a67cca43
Andrew S <andrewsheamls@gmail.com>
2022-03-15 17:21:35 -0600

    schema.sql built tables in employee database

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,26 @@
-drop database if exists employees_db;
+DROP DATABASE IF EXISTS employees_db;
 
-create database employees_db;
+CREATE DATABASE employees_db;
 
-use employees_db;
+USE employees_db;
 
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL
+)
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INT
+
+)
+
+CREATE TABLE dept (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    dept_name VARCHAR(30) NOT NULL
+)
--- index.js
+++ index.js
@@ -1,92 +1,89 @@
 const inquirer = require("inquirer");
 
 function mainPrompt() {
-    inquirer
-  .prompt({
-    type: "list",
-    message: "Make a selection.",
-    name: "selection",
-    choices: [
-      "view all departments",
-      "view all roles",
-      "view all employees",
-      "add a department",
-      "add a role",
-      "add an employee",
-      "update an employee role",
-      "quit the program",
-    ],
-  })
-  .then((data) => {
-    console.log(data);
-    if (data.selection === "view all departments") {
-      viewAllDepts();
-    } else if (data.selection === "view all roles") {
-        viewAllRoles()
-    } else if (data.selection === "view all employees") {
-        viewAllEmps()
-    } else if (data.selection === "add a department") {
-        addDept()
-    } else if (data.selection === "add a role") {
-        addRole()
-    } else if (data.selection === "add an employee") {
-        addEmployee()
-    } else if (data.selection === "update an employee role") {
-        updateEmployee()
-        } else {quitProgram()}
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      message: "Make a selection.",
+      name: "selection",
+      choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role",
+        "quit the program",
+      ],
+    })
+    .then((data) => {
+      console.log(data);
+      if (data.selection === "view all departments") {
+        viewAllDepts();
+      } else if (data.selection === "view all roles") {
+        viewAllRoles();
+      } else if (data.selection === "view all employees") {
+        viewAllEmps();
+      } else if (data.selection === "add a department") {
+        addDept();
+      } else if (data.selection === "add a role") {
+        addRole();
+      } else if (data.selection === "add an employee") {
+        addEmployee();
+      } else if (data.selection === "update an employee role") {
+        updateEmployee();
+      } else {
+        quitProgram();
+      }
+    });
 }
 
 function viewAllDepts() {
   console.log("get depts from database");
   //mysql to view depts from the database
-  mainPrompt()
+  mainPrompt();
 }
 
 function viewAllRoles() {
-    console.log("get roles from database");
-    //mysql to view depts from the database
-    mainPrompt()
-  }
+  console.log("get roles from database");
+  //mysql to view depts from the database
+  mainPrompt();
+}
 
-  function viewAllEmps() {
-    console.log("view all employees from database");
-    //mysql to view depts from the database
-    mainPrompt()
-  }
+function viewAllEmps() {
+  console.log("view all employees from database");
+  //mysql to view depts from the database
+  mainPrompt();
+}
 
-  function addDept() {
-    console.log("add dept to database");
-    //mysql to view depts from the database
-    mainPrompt()
-  }
+function addDept() {
+  console.log("add dept to database");
+  //mysql to view depts from the database
+  mainPrompt();
+}
 
-  function addRole() {
-    console.log("add role to database");
-    //mysql to view depts from the database
-    mainPrompt()
-  }
+function addRole() {
+  console.log("add role to database");
+  //mysql to view depts from the database
+  mainPrompt();
+}
 
-  function addEmployee() {
-    console.log("add employee to database");
-    //inquire .prompt here that asks the info about the employee
-    //mysql to view depts from the database
-    mainPrompt()
-  }
-  function updateEmployee() {
-    console.log("updates employee from database");
-    //mysql to view depts from the database
-    //choose an employee to update
-    mainPrompt()
-  }
-  function quitProgram() {
-    console.log("quits the program");
-    //mysql to view depts from the database
-    process.exit()
-  }
-  mainPrompt()
-  
-  
-  
-  
-  
+function addEmployee() {
+  console.log("add employee to database");
+  //inquire .prompt here that asks the info about the employee
+  //mysql to view depts from the database
+  mainPrompt();
+}
+function updateEmployee() {
+  console.log("updates employee from database");
+  //mysql to view depts from the database
+  //choose an employee to update
+  mainPrompt();
+}
+function quitProgram() {
+  console.log("quits the program");
+  //mysql to view depts from the database
+  process.exit();
+}
+mainPrompt();
