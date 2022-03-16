7dff5d0e179637e25f1f1b5cc8d61525186ca25f
Andrew S <andrewsheamls@gmail.com>
2022-03-15 19:23:31 -0600

    added info to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,39 @@
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES()
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+(
+    "Dennis", "Reynolds", --needs role_id and manager_id
+),
+
+(
+    "Charlie", "Kelly",--needs role_id and manager_id
+),
+
+(
+    "Ronald", "Macdonald",--needs role_id and manager_id
+),
+
+(
+    "Frank", "Reynolds",--needs role_id and manager_id
+),
+
+(
+    "Sweet Dee", "Reynolds" --needs rold_id and manager_id
+);
 
 
+INSERT INTO role (title, salary, dept_id)
+VALUES
+("Receptionst", 100000,) --needs dept_id
+("Designer", 110000, )--needs dept_id
+("Accountant", 120000, )--needs dept_id
+("Engineer", 130000, )--needs dept_id
+("Developer", 140000, )--needs dept_id
 
 
-INSERT INTO role (id, title, salary, dept_id)
-VALUES()
-
-
-
-
-INSERT INTO dept (id, dept_name)
-VALUES()
+INSERT INTO dept (dept_name)
+VALUES
+("Marketing"),
+("Finance"),
+("Human Resources"),
+("IT"),
+("Operations");
